684a7aad43625209a223213f3165b3313839e7c4
ainaProject <ainafitiavana.project@gmail.com>
2022-11-02 08:01:09 +0200

    pharmacy spécifique middleware

--- src/middlewares/admin.middleware.ts
+++ src/middlewares/admin.middleware.ts
@@ -5,7 +5,7 @@ import { UserEntity } from '@entities/users.entity';
 import { HttpException } from '@exceptions/HttpException';
 import { DataStoredInToken, RequestWithUser } from '@interfaces/auth.interface';
 
-const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+const adminAuthMiddleware = async (req: RequestWithUser, res: Response, next: NextFunction) => {
   try {
     const Authorization = req.cookies['Authorization'] || (req.header('Authorization') ? req.header('Authorization').split('Bearer ')[1] : null);
 
@@ -13,8 +13,9 @@ const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFun
       const secretKey: string = SECRET_KEY;
       const { id } = (await verify(Authorization, secretKey)) as DataStoredInToken;
       const findUser = await UserEntity.findOne({ where: { id: id }, relations: ['pharmacy', 'contact', 'userStatus', 'status'] });
-
-      if (findUser) {
+      const userStatus = findUser.userStatus.code;
+      if (userStatus === 'ADM' || userStatus === 'PHO') {
+        req.user = findUser;
         next();
       } else {
         next(new HttpException(401, 'Wrong authentication token'));
@@ -27,4 +28,4 @@ const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFun
   }
 };
 
-export default authMiddleware;
+export default adminAuthMiddleware;
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -3,6 +3,7 @@ import UsersController from '@controllers/users.controller';
 import { CreateUserDto } from '@dtos/users.dto';
 import { Routes } from '@interfaces/routes.interface';
 import validationMiddleware from '@middlewares/validation.middleware';
+import adminAuthMiddleware from '@/middlewares/admin.middleware';
 
 class UsersRoute implements Routes {
   public path = '/users';
@@ -14,11 +15,16 @@ class UsersRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.usersController.getUsers);
-    this.router.get(`${this.path}/:id(\\d+)`, this.usersController.getUserById);
-    this.router.post(`${this.path}`, validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
-    this.router.put(`${this.path}/:id(\\d+)`, validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
-    this.router.delete(`${this.path}/:id(\\d+)`, this.usersController.deleteUser);
+    this.router.get(`${this.path}`, adminAuthMiddleware, this.usersController.getUsers);
+    this.router.get(`${this.path}/:id(\\d+)`, adminAuthMiddleware, this.usersController.getUserById);
+    this.router.post(`${this.path}`, adminAuthMiddleware, validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
+    this.router.put(
+      `${this.path}/:id(\\d+)`,
+      adminAuthMiddleware,
+      validationMiddleware(CreateUserDto, 'body', true),
+      this.usersController.updateUser,
+    );
+    this.router.delete(`${this.path}/:id(\\d+)`, adminAuthMiddleware, this.usersController.deleteUser);
   }
 }
 
